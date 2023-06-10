d19e0bbe49ceae87845e45be37b37b6c9fbc1505
Surojit2023 <129222337+Surojit2023@users.noreply.github.com>
2023-06-10 00:55:44 -0400

    Update main.tf

--- terraform_code/dev/instances/main.tf
+++ terraform_code/dev/instances/main.tf
@@ -52,6 +52,7 @@ resource "aws_instance" "my_amazon" {
   key_name                    = aws_key_pair.my_key.key_name
   vpc_security_group_ids             = [aws_security_group.my_sg.id]
   associate_public_ip_address = false
+  user_data                   =<<EOF
 
   sudo yum update -y
   sudo yum install -y docker
